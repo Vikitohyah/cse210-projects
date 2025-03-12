4dded7a8dd3dd6a819faa6fc28c2759e32e41e37
Vikitohyah <victoriaedorh@gmail.com>
2025-03-12 14:18:13 +0100

    week two Resumes exercise completed

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,34 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        // Create the first job
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        // Create the second job
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        // Display job details using the method
+        job1.DisplayJobInfo();
+        job2.DisplayJobInfo();
+
+
+        // Create Resumes
+        Resume resume1 = new Resume();
+        resume1._name = "Allison Rose";
+
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        
+        // Display Resume details using the method
+        resume1.DisplayResume();
     }
 }
