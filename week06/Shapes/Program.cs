9f3f212928262e09e028b4f77ce57dd33fc1da59
Vikitohyah <victoriaedorh@gmail.com>
2025-04-12 20:04:06 +0100

    week six Shapes Activity completed

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        List<Shape> shapes = new List<Shape>();
+        Square s1 = new Square("Blue", 2);
+        shapes.Add(s1);
+
+        Rectangle r1 = new Rectangle("Purple", 5, 4);
+        shapes.Add(r1);
+
+        Circle c1 = new Circle("Yellow", 5);
+        shapes.Add(c1);
+
+        foreach(Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
+
+
     }
 }
