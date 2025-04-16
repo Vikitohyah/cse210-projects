f1d7c066f94ef51e8fd709a0d33030df6b685fa9
Vikitohyah <victoriaedorh@gmail.com>
2025-04-16 01:16:33 +0100

    week seven ExerciseTracking Activity classes added

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,30 @@
+public abstract class Exercise
+{
+    private DateTime _date;
+    private int _minutes;
+
+    public Exercise (DateTime date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public int GetMinutes()
+    {
+        return _minutes;
+
+    }
+    public DateTime GetDate()
+    {
+        return _date;
+    }
+
+    public abstract double GetDistance(); // km
+    public abstract double GetSpeed();    // kph
+    public abstract double GetPace();     // min per km
+
+    public virtual string GetSummary()
+    {
+        return $"{_date:dd MMM yyyy} {this.GetType().Name} ({_minutes} min) - Distance: {GetDistance():0.0} km, Speed: {GetSpeed():0.0} kph, Pace: {GetPace():0.00} min per km";
+    }
+}
